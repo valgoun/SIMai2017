2b8ff9f877db92773a9353fbb5ddbe18e9510c8a
Unknown <e.depaeuw@rubika-edu.com>
2017-05-17 13:58:49 +0200

    Tweak arena

--- Assets/Scripts/Character/RotatingControl.cs
+++ Assets/Scripts/Character/RotatingControl.cs
@@ -4,52 +4,6 @@ using UnityEngine;
 
 public class RotatingControl : MonoBehaviour
 {
-    public float ForceFactor = 1f;
-    private Rigidbody _body;
-    private SortedList<int, RotationPan> _pans = new SortedList<int, RotationPan>();
-    private CharacterControl _control;
-    // Use this for initialization
-    void Start()
-    {
-        _body = GetComponent<Rigidbody>();
-        _control = GetComponent<CharacterControl>();
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        var pan = other.GetComponentInParent<RotationPan>();
-        if (pan != null && !_pans.ContainsValue(pan))
-        {
-            _pans.Add(pan.Id, pan);
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        var pan = other.transform.GetComponentInParent<RotationPan>();
-        if (pan != null)
-        {
-            _pans.Remove(pan.Id);
-        }
-    }
-
-    void FixedUpdate()
-    {
-        if (_pans.Count <= 0 /*|| !_control.IsGrounded */|| _control.IsStuned)
-            return;
-
-
-        var circlePos = transform.position - _pans.Values[0].transform.position;
-        circlePos.y = 0;
-        var dist = circlePos.magnitude;
-        circlePos.Normalize();
-        var tangent = new Vector3(circlePos.z, 0, -circlePos.x);
-        _body.AddForce(tangent * (_pans.Values[0].Speed * Mathf.Max(dist, 1.0f) * ForceFactor));
-    }
-}
-
-public class RotatingControl : MonoBehaviour
-{
     //public float ForceFactor = 1f;
     private Rigidbody _body;
     private SortedList<int, RotationPan> _pans = new SortedList<int, RotationPan>();
