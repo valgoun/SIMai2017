2eaf819963168cc0c17cdf44dff73eacd5f6ed48
valgoun <valgoun@gmail.com>
2017-05-17 14:13:15 +0200

    Death Trigger and 4 player

--- Assets/Scripts/Character/CharacterControl.cs
+++ Assets/Scripts/Character/CharacterControl.cs
@@ -38,6 +38,7 @@ public class CharacterControl : MonoBehaviour
             return _isStunned;
         }
     }
+    //public Vector3
 
 
     private Player _player;
@@ -165,6 +166,19 @@ public class CharacterControl : MonoBehaviour
                 other.rigidbody.AddForce(_dashDirection * DashImpactForce, ForceMode.VelocityChange);
             }
             //other.rigidbody.DOMove(other.relativeVelocity.normalized * -DashImpactDistance, DashImpactSpeed).SetRelative().SetSpeedBased().SetEase(Ease.OutExpo);
+        }
+    }
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("DeathTrigger"))
+        {
+            GameManager.Instance.KillPlayer();
+            Destroy(gameObject);
         }
     }
 }
