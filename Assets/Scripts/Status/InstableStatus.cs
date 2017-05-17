19db6a9ed887951d68b957f1d24fa487bd674eef
MINARDI Morgan <m.minardi@rubika-edu.com>
2017-05-17 14:08:21 +0200

    désactivation des colliders lorsqu'ils sont descendus

--- Assets/Scripts/Status/InstableStatus.cs
+++ Assets/Scripts/Status/InstableStatus.cs
@@ -6,12 +6,15 @@ public class InstableStatus : MonoBehaviour {
 
     public float duration { get; set;}
 
+    private MeshCollider collider;
+
     private bool isFallen;
 
 	// Use this for initialization
 	void Start () {
         isFallen = false;
-        //duration = 1;
+        collider = GetComponent<MeshCollider>();
+        duration = 2.5f;
 	}
 	
 	// Update is called once per frame
@@ -33,9 +36,11 @@ public class InstableStatus : MonoBehaviour {
 
     IEnumerator fall()
     {
+        collider.enabled = false;
         transform.DOMoveY(-5, 0.25f).SetRelative();
         yield return new WaitForSeconds(duration);
         transform.DOMoveY(5, 0.25f).SetRelative();
+        collider.enabled = true;
         Destroy(this);
     }
 }
